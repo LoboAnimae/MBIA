52ed0585fccd60efa7aea3593f818ed51e493faf
LoboAnimae <andrese.quanl@gmail.com>
2024-02-26 18:00:08 -0600

    Finished project

--- DataMining/Exercise1/plots.r
+++ DataMining/Exercise1/plots.r
@@ -4,8 +4,19 @@ library(ggrepel)
 library(tidyr)
 load(file = "clean.Rdata")
 glimpse(data)
-
+file_path <- "./plots"
 one_million <- 1000000
+
+save <- function(name) {
+  ggsave(name,
+    path = file_path,
+    width = 4.5,
+    height = 4.5,
+    dpi = 300,
+    units = "in",
+    device = "png"
+  )
+}
 
 honey_tendency_national <- data %>%
   arrange(year) %>%
@@ -17,21 +28,74 @@ honey_tendency_state <- data %>%
   arrange(year) %>%
   summarize(total_produced_millions = sum(totalprod) / one_million)
 
+honey_tendency_north_dakota <- data %>%
+  group_by(state, year) %>%
+  filter(state == "ND") %>%
+  arrange(year) %>%
+  summarize(total_produced_millions = sum(totalprod) / one_million)
+
+honey_tendency_california <- data %>%
+  group_by(state, year) %>%
+  filter(state == "CA") %>%
+  arrange(year) %>%
+  summarize(total_produced_millions = sum(totalprod) / one_million)
+
 
 ggplot(honey_tendency_national, aes(x = year, y = total_produced_millions, group = 1)) +
   geom_line() +
+  geom_smooth(method = lm) +
   expand_limits(y = 0) +
   ggtitle("National Honey Production in Millions of Pounds") +
   xlab("Year") +
-  ylab("Millions of Pounds of Honey Produced")
+  ylab("Millions of Pounds of Honey Produced") + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+save("national_tendency.png")
 
-ggplot(honey_tendency_state, aes(x = year, y = total_produced_millions, color = state)) +
-  geom_line() +
+ggplot(honey_tendency_state, aes(x = year, y = total_produced_millions, color = state, group = state)) +
+  geom_line(show.legend = FALSE) +
   expand_limits(y = 0) +
   ggtitle("Statal Honey Production in Millions of Pounds") +
   xlab("Year") +
+  ylab("Millions of Pounds of Honey Produced")
+
+save("states_tendency.png")
+
+
+
+ggplot(honey_tendency_north_dakota, aes(x = year, y = total_produced_millions, color = state, group = state)) +
+  geom_line(color = "#005100") +
+  expand_limits(y = 0) +
+  ggtitle("North Dakota Honey Production in Millions of Pounds") +
+  xlab("Year") +
   ylab("Millions of Pounds of Honey Produced") +
-  geom_label_repel(aes(label = state),
-    nudge_x = 1,
-  )
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+save("states_tendency_north_dakota.png")
+
+
+ggplot(honey_tendency_california, aes(x = year, y = total_produced_millions, color = state, group = state)) +
+  geom_line(color = "#960606") +
+  expand_limits(y = 0) +
+  ggtitle("California Honey Production in Millions of Pounds") +
+  xlab("Year") +
+  ylab("Millions of Pounds of Honey Produced") + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+save("states_tendency_california.png")
+
+year_1998 <- data %>%
+  filter(year == 1998) %>%
+  group_by(state) %>%
+  arrange(desc(totalprod)) %>%
+  summarize(total_revenue_millions = sum(totalprod) / one_million)
+
+ggplot(year_1998, aes(x = reorder(state, total_revenue_millions), y = total_revenue_millions, fill = state)) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  expand_limits(y = 0) +
+  ggtitle("Total Revenue for Honey in 1998 per State") +
+  xlab("State") +
+  ylab("Value of Honey Produced in Millions") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+save("states_revenue_1998.png")
---
+++ DataMining/exam/code.r
@@ -0,0 +1,41 @@
+library(readxl)
+library(dplyr)
+library(e1071)
+library(rpart)
+library(randomForest)
+
+setwd("/Users/yagdrassyl/Documents/Code/University/MBIA/DataMining/exam")
+data <- read_excel("Datos_membresia.xlsx")
+
+# Check the data
+glimpse(data)
+
+# Desarrolle un modelo que logre clasificar la respuesta de los clientes
+# ante una invitación para suscribirse a una membresía en un club campestre...
+# (Utilizando los datos proporcionados en el archivo "Datos_membresia.xlsx".
+
+# Classify models: Logistic Regression, Decision Tree, Random Forest, SVM
+
+# naive Bayes
+
+set.seed(666)
+
+# Split the data into training and testing sets
+smp_size <- floor(0.70 * nrow(data))
+
+train_ind <- sample(seq_len(nrow(data)), size = smp_size)
+
+train <- data[train_ind, ]
+test <- data[-train_ind, ]
+
+naive_bayes_model <- naiveBayes(train$Acepta_membresia ~ ., data = train)
+predNB_Train <- predict(naive_bayes_model, newdata = train, type = "raw")
+predNB_Test <- predict(naive_bayes_model, newdata = test, type = "raw")
+
+
+# Decision Tree
+tree_model <- rpart(Acepta_membresia ~ ., data = train, method = "class")
+
+
+# Random Forest
+random_forest_model <- randomForest(Acepta_membresia ~ ., data = train, importance = F, type = "class")
